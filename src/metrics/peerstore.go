85ef9b25d94d5db4fdb06ef0be725b70957feffc
Cortze <mikelcortes013@gmail.com>
2021-09-30 17:21:40 +0200

    Prunning based on received error. Problems reading Pubkeys from peerstore

--- src/metrics/peerstore.go
+++ src/metrics/peerstore.go
@@ -12,6 +12,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type ErrorHandling func(*Peer)
+
 type PeerStore struct {
 	PeerStore         sync.Map
 	MessageDatabase   *database.MessageDatabase // TODO: Discuss
@@ -90,30 +92,45 @@ func (c *PeerStore) GetPeerData(peerId string) (Peer, error) {
 	return peerData.(Peer), nil
 }
 
-// Add a connection attempt event to the peer with the result of this one
-func (c *PeerStore) AddConnectionAttempt(id string, succeed bool, rec_err string) error {
+/// AddNewAttempts adds the resuts of a negative new attempt over an existing peer
+// increasing the attempt counter and the respective fields
+func (c *PeerStore) AddNewNegConnectionAttempt(id string, rec_err string, fn ErrorHandling) error {
 	p, err := c.GetPeerData(id)
-	if err != nil {
+	if err != nil { // the peer was already in the sync.Map return true
 		return fmt.Errorf("Not peer found with that ID %s", id)
 	}
 	// Update the counter and connection status
+	p.Attempts += 1
 	if !p.Attempted {
 		p.Attempted = true
-		//fmt.Println("Original ", err)
-		// MIGHT be nice to try if we can change the uncertain errors for the dial backoff
-		if rec_err != "" || rec_err != "dial backoff" {
-			p.Error = utils.FilterError(rec_err)
-		}
-	}
-	p.AddNegativeConnAttempt()
-	if succeed {
-		p.Succeed = succeed
-		p.Error = "None"
-		// clean the Negative connection Attempt list
-		p.AddPositiveConnAttempt()
-	}
-	p.Attempts += 1
+		p.Error = utils.FilterError(rec_err)
 
+	}
+	// Handle each of the different error types as defined currently at pruneconnect.go
+	fn(&p)
+
+	// Store the new struct in the sync.Map
+	c.StorePeer(p)
+	return nil
+}
+
+// AddNewPosConnectionAttempt adds the resuts of a possitive new attempt over an existing peer
+// increasing the attempt counter and the respective fields
+func (c *PeerStore) AddNewPosConnectionAttempt(id string) error {
+	p, err := c.GetPeerData(id)
+	if err != nil { // the peer was already in the sync.Map return true
+		return fmt.Errorf("Not peer found with that ID %s", id)
+	}
+	// Update the counter and connection status
+	p.Attempts += 1
+	if !p.Attempted {
+		p.Attempted = true
+
+	}
+	p.Succeed = true
+	p.Error = "None"
+	// clean the Negative connection Attempt list
+	p.AddPositiveConnAttempt()
 	// Store the new struct in the sync.Map
 	c.StorePeer(p)
 	return nil
