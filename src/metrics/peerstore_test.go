0eb1a8cfe2451100fe0f467faf4b75590702af18
Tarun <tarsuno@gmail.com>
2021-10-19 17:07:42 +0200

    Adding metrics

---
+++ src/metrics/peerstore_test.go
@@ -0,0 +1,37 @@
+package metrics
+
+import (
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func Test_StoreOrUpdatePeer(t *testing.T) {
+	// stores a peer
+	peerStore := NewPeerStore("memory", "")
+	p1 := NewPeer("Peer1")
+	p1.ClientName = "Client1"
+	peerStore.StoreOrUpdatePeer(p1)
+
+	// rx some messages of topic1
+	err := peerStore.MessageEvent("Peer1", "topic1")
+	require.NoError(t, err)
+	err = peerStore.MessageEvent("Peer1", "topic1")
+	require.NoError(t, err)
+
+	// check that its data is correct
+	p, err := peerStore.GetPeerData("Peer1")
+	require.NoError(t, err)
+	require.Equal(t, p.ClientName, "Client1")
+	require.Equal(t, p.MessageMetrics["topic1"].Count, uint64(2))
+
+	// add a new version of the same peer
+	p1Mod := NewPeer("Peer1")
+	p1Mod.ClientName = "NewClient"
+	peerStore.StoreOrUpdatePeer(p1Mod)
+	p, err = peerStore.GetPeerData("Peer1")
+	require.NoError(t, err)
+
+	// check that the new information was updated
+	require.Equal(t, p.ClientName, "NewClient")
+	require.Equal(t, p.MessageMetrics["topic1"].Count, uint64(2))
+}
