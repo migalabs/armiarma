a8e21b5c996b1df335465928056c99a0212c551b
cortze <mikelcortes013@gmail.com>
2021-10-26 15:57:22 +0200

    Include req_host_info + Custom conn notify

--- src/hosts/notify.go
+++ src/hosts/notify.go
@@ -1,8 +1,14 @@
 package hosts
 
 import (
+	"context"
+	"time"
+
+	eth_node "github.com/ethereum/go-ethereum/p2p/enode"
 	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/migalabs/armiarma/src/db"
 	ma "github.com/multiformats/go-multiaddr"
+	"github.com/sirupsen/logrus"
 )
 
 /*
@@ -18,9 +24,60 @@ func (c *BasicLibp2pHost) standardListenCloseF(net network.Network, addr ma.Mult
 }
 
 func (c *BasicLibp2pHost) standardConnectF(net network.Network, conn network.Conn) {
-	c.Log.Debug("Connection")
-	// c.Log.Debug(fmt.Sprintf("%+v\n", conn))
-	// c.Log.Debugf("%+v\n", c.Host().Network().Peerstore().Peers())
+	c.Log.WithFields(logrus.Fields{
+		"EVENT":     "Connection detected",
+		"DIRECTION": conn.Stat().Direction.String(),
+	}).Debug("Peer: ", conn.RemotePeer().String())
+	// Generate new peer to aggregate new data
+	peer := db.NewPeer(conn.RemotePeer().String())
+	h, _ := c.Host()
+	// Request the Host Metadata
+	err := ReqHostInfo(context.Background(), h, conn, &peer)
+	if err != nil {
+		peer.MetadataSucceed = false
+		c.Log.WithFields(logrus.Fields{
+			"ERROR": err,
+		}).Warn("Peer: ", conn.RemotePeer().String())
+	}
+	/* ---- TODO: Generate/Import the RPC calls for Eth2 to support the given RPC call methods
+	// Request the BeaconMetadata
+	bMetadata, err := ReqBeaconMetadata(context.Background(), h, conn.RemotePeer())
+	if err != nil {
+		c.Log.WithFields(logrus.Fields{
+			"ERROR": err,
+		}).Debug("Peer: ", conn.RemotePeer().String())
+	} else {
+		peer.UpdateBeaconMetadata(bMetadata)
+	}
+	// request BeaconStatus metadata as we connect to a peer
+	bStatus, err := ReqBeaconStatus(context.Background(), h, conn.RemotePeer())
+	if err != nil {
+		c.Log.WithFields(logrus.Fields{
+			"ERROR": err,
+		}).Debug("Peer: ", conn.RemotePeer().String())
+	} else {
+		peer.UpdateBeaconStatus(bStatus)
+	}
+	*/
+	// Read ENR of the Peer from the generated enode
+	n := c.Store.LatestENR(conn.RemotePeer())
+	if n != nil {
+		n.ID().String()
+		// We can only get the node.ID if the ENR of the peer was already in the PeerStore fromt dv5
+		peer.NodeId = n.ID().String()
+	} else {
+		edcsakey, err := eth_node.ParsePubkey(peer.Pubkey)
+		if err != nil {
+			c.Log.WithFields(logrus.Fields{
+				"ERROR": "parsing pubkey",
+			}).Debug("Peer: ", conn.RemotePeer().String())
+		}
+		peer.NodeId = eth_node.PubkeyToIDV4(edcsakey).String()
+	}
+	// Add new connection event
+	peer.ConnectionEvent(conn.Stat().Direction.String(), time.Now())
+	// Add new peer or aggregate info to existing peer
+	c.PeerStore.StoreOrUpdatePeer(peer)
 }
 
 func (c *BasicLibp2pHost) standardDisconnectF(net network.Network, conn network.Conn) {
