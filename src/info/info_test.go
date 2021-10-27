ad581feef519fe7050913aaf4196e4efa3951602
Tarun <tarsuno@gmail.com>
2021-10-27 14:15:44 +0200

    Moved input validations to Info

--- src/info/info_test.go
+++ src/info/info_test.go
@@ -1,28 +1,44 @@
 package info
 
 import (
-	"fmt"
+	"strings"
 	"testing"
 
-	"github.com/migalabs/armiarma/src/config"
+	"github.com/migalabs/armiarma/src/base"
 	"github.com/stretchr/testify/require"
 )
 
-func TestReadJSON(t *testing.T) {
-	config_object := config.NewDefaultConfigData()
-
-	err := config_object.ReadFromJSON("custom.json")
-
-	if err != nil {
-		fmt.Println(err)
+func Test_CustomInfoDataSuccess(t *testing.T) {
+	stdOpts := base.LogOpts{
+		Output:    "terminal",
+		Formatter: "text",
 	}
+	info_object := NewCustomInfoData("../config/config_success.json", stdOpts)
 
-	require.Equal(t, config_object.GetIP(), "127.0.0.1")
-	require.Equal(t, config_object.GetTcpPort(), 100)
-	require.Equal(t, config_object.GetUdpPort(), 101)
-	require.Equal(t, config_object.GetTopicArray(), []string{"echo", "ohce"})
-	require.Equal(t, config_object.GetNetwork(), "testnet")
-	require.Equal(t, config_object.GetForkDigest(), "0xdlskgfn")
-	require.Equal(t, config_object.GetUserAgent(), "bsc_test")
+	require.Equal(t, info_object.GetIP().String(), "127.0.0.1")
+	require.Equal(t, info_object.GetTcpPort(), 100)
+	require.Equal(t, info_object.GetUdpPort(), 101)
+	require.Equal(t, info_object.GetTopicArray(), []string{"echo", "ohce"})
+	require.Equal(t, info_object.GetNetwork(), "testnet")
+	require.Equal(t, info_object.GetForkDigest(), "0xdlskgfn")
+	require.Equal(t, info_object.GetUserAgent(), "bsc_test")
+	require.Equal(t, info_object.GetPrivKeyString(), "026c60367b01fe3d7c7460bce1d585260ce465fa0abcb6e13619f88bf0dad54f")
+
+}
+
+func Test_CustomInfoDataFail(t *testing.T) {
+	stdOpts := base.LogOpts{
+		Output:    "terminal",
+		Formatter: "text",
+	}
+	info_object := NewCustomInfoData("../config/config_fail.json", stdOpts)
+
+	require.Equal(t, info_object.GetIP().String(), "127.0.0.1")
+	require.Equal(t, info_object.GetTcpPort(), 100)
+	require.Equal(t, info_object.GetUdpPort(), 101)
+	require.Equal(t, len(info_object.GetTopicArray()), len(strings.Split(DefaultTopicArray, ",")))
+	require.Equal(t, info_object.GetNetwork(), "testnet")
+	require.Equal(t, info_object.GetForkDigest(), DefaultForkDigest)
+	require.Equal(t, info_object.GetUserAgent(), "bsc_test")
 
 }
