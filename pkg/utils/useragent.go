94f58190969aab74d7cc4ba310a142327e873f30
cortze <mikelcortes013@gmail.com>
2022-11-16 11:20:55 +0100

    Update the UserAgent parser - OS + Arch+

--- pkg/utils/useragent.go
+++ pkg/utils/useragent.go
@@ -4,6 +4,251 @@ import (
 	"strings"
 )
 
+type P2pNetwork string
+type ClientName string
+type ClientOS string
+type ClientArch string
+
+const (
+	// Libp2p Available Networks
+	EthereumNetwork P2pNetwork = "Ethereum CL"
+	IpfsNetwork     P2pNetwork = "IPFS"
+	FilecoinNetwork P2pNetwork = "Filecoin"
+
+	// Ethereum Consensus-Layer Clients
+	Prysm      ClientName = "prysm"
+	Lighthouse ClientName = "lighthouse"
+	Teku       ClientName = "teku"
+	Nimbus     ClientName = "nimbus"
+	Lodestar   ClientName = "lodestar"
+	Grandine   ClientName = "grandine"
+	Cortex     ClientName = "cortze"
+	Trinity    ClientName = "trinity"
+
+	// IPFS Client
+	Kubo         ClientName = "kubo"
+	GoIpfs       ClientName = "go-ipfs"
+	HydraBooster ClientName = "hydra-booster"
+	Storm        ClientName = "storm"
+	Ioi          ClientName = "ioi"
+	Punchr       ClientName = "punchr"
+
+	// Filecoin
+	Lotus ClientName = "lotus"
+
+	// Others
+	Others ClientName = "Others"
+
+	// OSes
+	Mac     ClientOS = "mac"
+	Windows ClientOS = "windows"
+	Linux   ClientOS = "linux"
+
+	// Arch
+	Arm    ClientArch = "arm"
+	X86_64 ClientArch = "x86_64"
+
+	Unknown string = "unknown"
+)
+
+// Ethereum CL CLients
+var EthCLClients map[ClientName][]string = map[ClientName][]string{
+	Prysm:      {"prysm"},
+	Lighthouse: {"lighthouse"},
+	Teku:       {"teku"},
+	Nimbus:     {"nimbus", "nim-libp2p"},
+	Lodestar:   {"lodestar", "js-libp2p"},
+	Grandine:   {"grandine", "rust-libp2p"},
+	Cortex:     {"cortex"},
+	Trinity:    {"trinity"},
+}
+
+// IPFS Clients
+var IpfsClients map[ClientName][]string = map[ClientName][]string{
+	Kubo:         {"kubo"},
+	GoIpfs:       {"go-ipfs"},
+	HydraBooster: {"hydra-booster"},
+	Storm:        {"storm"},
+	Ioi:          {"ioi"},
+	Punchr:       {"punchr"},
+}
+
+// Filecoin Clients
+var FilecoinClients map[ClientName][]string = map[ClientName][]string{
+	Lotus: {"lotus"},
+}
+
+// Valid OS
+var ValidOs map[ClientOS][]string = map[ClientOS][]string{
+	Mac:     {"macos", "freebsd"},
+	Windows: {"win", "windows"},
+	Linux:   {"linux", "ubuntu"},
+}
+
+// Valid Architectures
+var ValidArchs map[ClientArch][]string = map[ClientArch][]string{
+	Arm:    {"aarch64", "aarch", "aarch_64"},
+	X86_64: {"x86_64"},
+}
+
+// Examples:
+// Teku: teku/teku/v21.8.2/linux-x86_64/corretto-java-16
+// Teku: teku/teku/v21.7.0+9-g77b4b9e/linux-x86_64/-ubuntu-openjdk64bitservervm-java-11
+// Prysm: Prysm/v1.4.3/8bca66ac6408a03af52d65541f58384007ed50ef
+// Prysm: Prysm/v1.3.8-hotfix+6c0942/6c09424feb3141b96016bed817d7ade1cd75deb7
+// Lighthouse: Lighthouse/v1.5.1-b0ac346/x86_64-linux
+// Nimbus: nimbus
+// go-ipfs: go-ipfs/0.8.0/48f94e2
+// hydra-boost: hydra-booster/0.7.4
+// storm: storm
+// lotus: lotus-1.13.0+mainnet+git.7a55e8e8
+
+// TODO: Add network to the
+func ParseClientType(network P2pNetwork, userAgent string) (cliName string, cliVersion string, cliOs string, cliArch string) {
+
+	// split the UserAgent into chunks divided by '/'
+	splUserAgent := strings.Split(userAgent, "/")
+
+	// check the which of the clients is present in the userAgent
+	switch network {
+	case EthereumNetwork:
+		// parse client name from Ethereum Valid Clients
+		client := ClientNameParser(EthCLClients, splUserAgent[0])
+
+		// stract the version from the user
+		var version string
+		switch client {
+		case Prysm, Lighthouse, Lodestar, Grandine, Nimbus, Cortex, Trinity:
+			version = cleanVersion(getVersionIfAny(splUserAgent, 1))
+		case Teku:
+			// teku
+			version = cleanVersion(getVersionIfAny(splUserAgent, 2))
+
+		default:
+			log.Errorf("unable to determine client name for UserAgent %s", userAgent)
+			version = Unknown
+		}
+
+		cliName = string(client)
+		cliVersion = version
+
+	case IpfsNetwork:
+		// parse client name from Ethereum Valid Clients
+		client := ClientNameParser(IpfsClients, splUserAgent[0])
+
+		// stract the version from the user
+		var version string
+		switch client {
+		case GoIpfs, Kubo, Ioi, Storm, HydraBooster:
+			version = cleanVersion(getVersionIfAny(splUserAgent, 1))
+		default:
+			log.Errorf("unable to determine client name for UserAgent %s", userAgent)
+			version = Unknown
+		}
+
+		cliName = string(client)
+		cliVersion = version
+
+	case FilecoinNetwork:
+		// parse client name from Ethereum Valid Clients
+		client := ClientNameParser(FilecoinClients, splUserAgent[0])
+
+		// stract the version from the user
+		var version string
+		switch client {
+		case GoIpfs, Kubo, Ioi, Storm, HydraBooster:
+			version = cleanVersion(cleanVersionLotus(splUserAgent[0]))
+		default:
+			log.Errorf("unable to determine client name for UserAgent %s", userAgent)
+			version = Unknown
+		}
+
+		cliName = string(client)
+		cliVersion = version
+
+	default:
+		log.Error("unable to retrieve the ")
+	}
+
+	os := ClientOSParser(ValidOs, userAgent)
+	arch := ClientArchParser(ValidArchs, userAgent)
+
+	cliOs = string(os)
+	cliArch = string(arch)
+
+	return
+}
+
+func ClientNameParser(validNames map[ClientName][]string, parsingName string) ClientName {
+	defaultName := ClientName(Unknown)
+
+	// iter over the possibilities for the ethereum consensus layer
+	for cName, subCliNames := range validNames {
+		// iter through sub-cli names (e.g. lodestar and js-libp2p)
+		for _, subValidCli := range subCliNames {
+			if strContainsLowerCaps(string(parsingName), string(subValidCli)) {
+				return cName
+			}
+		}
+	}
+	return defaultName
+}
+
+func ClientOSParser(validNames map[ClientOS][]string, parsingName string) ClientOS {
+	defaultName := ClientOS(Unknown)
+
+	// iter over the possibilities for the CPU architecture
+	for os, subOS := range validNames {
+		// iter through sub-cli names (e.g. windows and linux)
+		for _, subValidOS := range subOS {
+			if strContainsLowerCaps(string(parsingName), string(subValidOS)) {
+				return os
+			}
+		}
+	}
+	return defaultName
+}
+
+func ClientArchParser(validNames map[ClientArch][]string, parsingName string) ClientArch {
+	defaultName := ClientArch(Unknown)
+
+	// iter over the possibilities for the ethereum consensus layer
+	for arch, subArchNames := range validNames {
+		// iter through sub-cli names (e.g. lodestar and js-libp2p)
+		for _, subValidOS := range subArchNames {
+			if strContainsLowerCaps(string(parsingName), string(subValidOS)) {
+				return arch
+			}
+		}
+	}
+	return defaultName
+}
+
+func strContainsLowerCaps(s string, subStr string) bool {
+	return strings.Contains(strings.ToLower(s), strings.ToLower(subStr))
+}
+
+func getVersionIfAny(fields []string, index int) string {
+	if index > (len(fields) - 1) {
+		return Unknown
+	} else {
+		return fields[index]
+	}
+}
+
+func cleanVersion(version string) string {
+	cleaned := strings.Split(version, "+")[0]
+	cleaned = strings.Split(cleaned, "-")[0]
+	return cleaned
+}
+
+func cleanVersionLotus(version string) string {
+	cleaned := strings.Split(version, "+")[0]
+	cleaned = strings.Split(cleaned, "-")[1]
+	return cleaned
+}
+
+// ------>   Deprecated for upper logic ^   <--------
 // Gets the client and version for a given userAgent.
 // TODO: Perhaps use some regex
 func FilterClientType(userAgent string) (string, string) {
@@ -45,31 +290,11 @@ func FilterClientType(userAgent string) (string, string) {
 		return "storm", cleanVersion(getVersionIfAny(fields, 1))
 	} else if strings.Contains(userAgentLower, "lotus") { // IPFS
 		// TODO: wont work, needs to be fixed to get the real Version
-		return "lotus", cleanVersionAux(getVersionIfAny(fields, 0))
+		return "lotus", cleanVersionLotus(getVersionIfAny(fields, 0))
 	} else if userAgentLower == "" {
 		return "NotIdentified", ""
 	} else {
 		log.Debugf("Could not get client from userAgent: %s", userAgent)
 		return "Others", ""
 	}
-}
-
-func getVersionIfAny(fields []string, index int) string {
-	if index > (len(fields) - 1) {
-		return "Unknown"
-	} else {
-		return fields[index]
-	}
-}
-
-func cleanVersion(version string) string {
-	cleaned := strings.Split(version, "+")[0]
-	cleaned = strings.Split(cleaned, "-")[0]
-	return cleaned
-}
-
-func cleanVersionAux(version string) string {
-	cleaned := strings.Split(version, "+")[0]
-	cleaned = strings.Split(cleaned, "-")[1]
-	return cleaned
 }
--- pkg/utils/useragent_test.go
+++ pkg/utils/useragent_test.go
@@ -1,74 +1,128 @@
 package utils
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+type clientInfoTest struct {
+	userAgent     string
+	clientName    string
+	clientVersion string
+	clientOS      string
+	clientArch    string
+}
+
+var Eth2TestClients []clientInfoTest = []clientInfoTest{
+	{
+		userAgent:     "teku/teku/v21.8.2/linux-x86_64/corretto-java-16",
+		clientName:    "teku",
+		clientVersion: "v21.8.2",
+		clientOS:      "linux",
+		clientArch:    "x86_64",
+	},
+	{
+		userAgent:     "teku/teku/v21.7.0+9-g77b4b9e/linux-x86_64/-ubuntu-openjdk64bitservervm-java-11",
+		clientName:    "teku",
+		clientVersion: "v21.7.0",
+		clientOS:      "linux",
+		clientArch:    "x86_64",
+	},
+	{
+		userAgent:     "Prysm/v1.4.3/8bca66ac6408a03af52d65541f58384007ed50ef",
+		clientName:    "prysm",
+		clientVersion: "v1.4.3",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "Prysm/v1.3.8-hotfix+6c0942/6c09424feb3141b96016bed817d7ade1cd75deb7",
+		clientName:    "prysm",
+		clientVersion: "v1.3.8",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "Lighthouse/v1.5.1-b0ac346/x86_64-linux",
+		clientName:    "lighthouse",
+		clientVersion: "v1.5.1",
+		clientOS:      "linux",
+		clientArch:    "x86_64",
+	},
+	{
+		userAgent:     "Lighthouse/v3.1.2/aarch64-macos",
+		clientName:    "lighthouse",
+		clientVersion: "v3.1.2",
+		clientOS:      "mac",
+		clientArch:    "arm",
+	},
+	{
+		userAgent:     "Lighthouse/v2.5.1-df51a73/aarch64-linux",
+		clientName:    "lighthouse",
+		clientVersion: "v2.5.1",
+		clientOS:      "linux",
+		clientArch:    "arm",
+	},
+	{
+		userAgent:     "nimbus",
+		clientName:    "nimbus",
+		clientVersion: "unknown",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "rust-libp2p/0.36.1",
+		clientName:    "grandine",
+		clientVersion: "0.36.1",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "js-libp2p/0.36.2",
+		clientName:    "lodestar",
+		clientVersion: "0.36.2",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "lodestar/v1.2.0",
+		clientName:    "lodestar",
+		clientVersion: "v1.2.0",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+	{
+		userAgent:     "nim-libp2p/0.0.1",
+		clientName:    "nimbus",
+		clientVersion: "0.0.1",
+		clientOS:      "unknown",
+		clientArch:    "unknown",
+	},
+}
+
+// TODO: Fill it with the
+var IPFSTestClients []string = []string{
+	"go-ipfs/0.8.0/48f94e2",
+	"hydra-booster/0.7.4",
+	"storm",
+	"kubo/0.15.0-dev/",
+	"ioi",
+	"punchr/honeypot/dev+",
+}
+
+var FilecoinTestClients []string = []string{
+	"lotus-1.13.0+mainnet+git.7a55e8e8",
+}
+
 func Test_FilterClientType(t *testing.T) {
-	client, version := FilterClientType("teku/teku/v21.8.2/linux-x86_64/corretto-java-16")
-	require.Equal(t, client, "Teku")
-	require.Equal(t, version, "v21.8.2")
-
-	client, version = FilterClientType("teku/teku/v21.7.0+9-g77b4b9e/linux-x86_64/-ubuntu-openjdk64bitservervm-java-11")
-	require.Equal(t, client, "Teku")
-	require.Equal(t, version, "v21.7.0")
-
-	client, version = FilterClientType("Prysm/v1.4.3/8bca66ac6408a03af52d65541f58384007ed50ef")
-	require.Equal(t, client, "Prysm")
-	require.Equal(t, version, "v1.4.3")
-
-	client, version = FilterClientType("Prysm/v1.3.8-hotfix+6c0942/6c09424feb3141b96016bed817d7ade1cd75deb7")
-	require.Equal(t, client, "Prysm")
-	require.Equal(t, version, "v1.3.8")
-
-	client, version = FilterClientType("Lighthouse/v1.5.1-b0ac346/x86_64-linux")
-	require.Equal(t, client, "Lighthouse")
-	require.Equal(t, version, "v1.5.1")
-
-	client, version = FilterClientType("Lighthouse/v2.0.0-7c88f58/x86_64-linux")
-	require.Equal(t, client, "Lighthouse")
-	require.Equal(t, version, "v2.0.0")
-
-	client, version = FilterClientType("nimbus")
-	require.Equal(t, client, "Nimbus")
-	require.Equal(t, version, "Unknown")
-
-	client, version = FilterClientType("rust-libp2p/0.31.0")
-	require.Equal(t, client, "Grandine")
-	require.Equal(t, version, "0.31.0")
-
-	client, version = FilterClientType("eth2-crawler")
-	require.Equal(t, client, "NodeWatch")
-	require.Equal(t, version, "")
-
-	client, version = FilterClientType("go-ipfs/0.8.0/48f94e2")
-	require.Equal(t, client, "go-ipgs")
-	require.Equal(t, version, "0.8.0")
-
-	client, version = FilterClientType("hydra-booster/0.7.4")
-	require.Equal(t, client, "hydra-boost")
-	require.Equal(t, version, "0.7.4")
-
-	client, version = FilterClientType("storm")
-	require.Equal(t, client, "storm")
-	require.Equal(t, version, "Unknown")
-
-	client, version = FilterClientType("lotus-1.13.0+mainnet+git.7a55e8e8")
-	require.Equal(t, client, "lotus")
-	require.Equal(t, version, "1.13.0")
-
-	client, version = FilterClientType("armiarma-crawler")
-	require.Equal(t, client, "BSC-Crawler")
-	require.Equal(t, version, "")
-
-	client, version = FilterClientType("bsc-crawler")
-	require.Equal(t, client, "BSC-Crawler")
-	require.Equal(t, version, "")
-
-	// Check it doesn't break for an unexpected agent
-	client, version = FilterClientType("")
-	require.Equal(t, client, "NotIdentified")
-	require.Equal(t, version, "")
+	for _, cliInf := range Eth2TestClients {
+		fmt.Println(cliInf)
+		client, version, os, arch := ParseClientType(EthereumNetwork, cliInf.userAgent)
+		require.Equal(t, client, cliInf.clientName)
+		require.Equal(t, version, cliInf.clientVersion)
+		require.Equal(t, os, cliInf.clientOS)
+		require.Equal(t, arch, cliInf.clientArch)
+	}
 }
